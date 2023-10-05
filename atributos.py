9e6d0f56d6a9e400dbc586e949f35e98b63679ad
Mauricio Peña <emauriciope@gmail.com>
2023-10-05 14:42:44 -0500

    1er cambio

---
+++ atributos.py
@@ -0,0 +1,9 @@
+class Alumno:
+    def __init__(self, nombre, nota) -> None:
+        self.nombre = nombre
+        self.nota = nota
+    def imprimir(self):
+        print("")  
+        print("") 
+        print("")  
+
